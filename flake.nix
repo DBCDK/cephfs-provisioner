f8e8496ae683f7fd0e7905969f827169afbfe5b4
Jonathan Zielinski <jozi@dbc.dk>
2023-07-27 13:55:09 +0200

    flake.nix: add overlay

--- flake.nix
+++ flake.nix
@@ -10,44 +10,53 @@
     self,
     flake-parts,
     ...
-  }:
+  }: let
+    package = "cephfs-provisioner";
+    provisionCmdPath = "usr/local/bin";
+  in
     flake-parts.lib.mkFlake {inherit inputs;} {
+      flake.overlays.default = final: prev: let
+        python = final.python39;
+      in {
+        "${package}" = final.buildGoModule {
+          version = "0.1";
+          pname = package;
+          src = ./.;
+          vendorSha256 = "sha256-DD73LLQYR/DWolZZUeNO8c93Vnknbjgn5A1UUWs+b+Q=";
+
+          buildInputs = [final.makeWrapper];
+          propagatedBuildInputs = [python final.ceph];
+
+          postPatch = let
+            pyLibPath = "${final.ceph}/lib/${python.libPrefix}/site-packages";
+          in ''
+            set -e
+            mkdir -p $out/${provisionCmdPath}
+            cp cephfs_provisioner/cephfs_provisioner.py $out/${provisionCmdPath}/cephfs_provisioner
+            patchShebangs $out/
+            stat ${pyLibPath}
+            wrapProgram "$out/${provisionCmdPath}/cephfs_provisioner" --set PYTHONPATH ${pyLibPath}
+          '';
+        };
+      };
       systems = ["x86_64-linux"];
 
       perSystem = {
         self',
         inputs',
         pkgs,
+        system,
         ...
-      }: let
-        package = "cephfs-provisioner";
-        python = pkgs.python39;
-        provisionCmdPath = "usr/local/bin";
-      in {
-        packages = {
-          default = self'.packages."${package}";
-
-          "${package}" = pkgs.buildGoModule {
-            version = "0.1";
-            pname = package;
-            src = ./.;
-            vendorSha256 = "sha256-DD73LLQYR/DWolZZUeNO8c93Vnknbjgn5A1UUWs+b+Q=";
-
-            buildInputs = [ pkgs.makeWrapper ];
-            propagatedBuildInputs = [ python pkgs.ceph ];
-
-            postPatch = let
-              pyLibPath = "${pkgs.ceph}/lib/${python.libPrefix}/site-packages";
-            in ''
-              set -e
-              mkdir -p $out/${provisionCmdPath}
-              cp cephfs_provisioner/cephfs_provisioner.py $out/${provisionCmdPath}/cephfs_provisioner
-              patchShebangs $out/
-              stat ${pyLibPath}
-              wrapProgram "$out/${provisionCmdPath}/cephfs_provisioner" --set PYTHONPATH ${pyLibPath}
-            '';
-          };
+      }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            self.overlays.default
+          ];
+          config = {};
         };
+
+        packages.default = pkgs.${package};
 
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
